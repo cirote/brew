179f679c007d5d4f9101503cd24a42797c000d27
cirote <estebanrogel@gmail.com>
2019-08-22 00:47:51 -0300

    Prueba de facade

--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -15,6 +15,7 @@ class RecetaController extends Controller
      */
     public function index()
     {
+        dd(JeJe::Density());
         return view('recetas.index')
             ->with('recetas', Receta::orderBy('nombre')->get());
     }
