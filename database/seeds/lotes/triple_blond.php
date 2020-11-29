7336b9515ede987a9e5a9081f14205bcd0ffc3d4
cirote <estebanrogel@gmail.com>
2020-11-29 16:08:43 -0300

    Agrega lupulo

--- database/seeds/lotes/triple_blond.php
+++ database/seeds/lotes/triple_blond.php
@@ -30,9 +30,10 @@ class triple_blond extends Seeder
 //                ->densidad(Scalar::Density('1.051 sg'))
             ->hervir(CarbonInterval::minutes(60))
                 ->lupulo(Lupulo::byNombre('Columbus'), Scalar::Weight('12.79 g'), CarbonInterval::minutes(114), 17.3)
-                ->lupulo(Lupulo::byNombre('Magnum'), Scalar::Weight('1.63 g'), CarbonInterval::minutes(25), 12.1)
-                ->lupulo(Lupulo::byNombre('Cascade'), Scalar::Weight('10.03 g'), CarbonInterval::minutes(25), 6.4)
-            ->final(Scalar::Volume('19.0 l'))
+                ->lupulo(Lupulo::byNombre('Perle'), Scalar::Weight('12.42 g'), CarbonInterval::minutes(25), 12.1)
+                ->lupulo(Lupulo::byNombre('Cascade'), Scalar::Weight('13.03 g'), CarbonInterval::minutes(25), 6.4)
+                ->lupulo(Lupulo::byNombre('Hallertauer Mittelfruh'), Scalar::Weight('10.25 g'), CarbonInterval::minutes(25), 12.1)
+           ->final(Scalar::Volume('19.0 l'))
             ->fermentar([
                 'fermentador' => 'Anvil 7.5 gl',
                 'volumen' => Scalar::Volume('18.5 l'),
